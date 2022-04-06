9bb2177dc29916f97ef42eef84e09ad88bc60ec3
tun71400 <77815590+tun71400@users.noreply.github.com>
2022-04-05 21:45:27 -0400

    Update ProductsController.cs
    update

--- ProductsApp2/Controllers/ProductsController.cs
+++ ProductsApp2/Controllers/ProductsController.cs
@@ -12,9 +12,9 @@ namespace ProductsApp2.Controllers
 
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Chicken Soup", Category = "Food", Price = 5 },
+            new Product { Id = 2, Name = "Pencil", Category = "Office Supply", Price = 10 },
+            new Product { Id = 3, Name = "Elden Ring", Category = "Game", Price = 15 }
         };
 
         public ProductsController()
